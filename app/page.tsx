822f4bdf694e2cc2f45a8cc2db15a030f6d23bea
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-05-25 14:52:53 +0530

    Refactor components and utils: Update component structure and add utility functions for color and class merging

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,52 @@
-import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>
-          Let's get started
-      </Button>
-    </div>
-  )
-}
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard 
+          id="123"
+          name="Companion 1"
+          topic="Topic 1"
+          subject="Subject 1"
+          duration={30}
+          color="#ffda6e"
 
-export default Page
+        />
+        <CompanionCard 
+          id="456"
+          name="Companion 2"
+          topic="Topic 2"
+          subject="Subject 2"
+          duration={45}
+          color="#e5d0ff"
+
+        />
+        <CompanionCard 
+          id="789"
+          name="Companion 3"
+          topic="Topic 3"
+          subject="Subject 3"
+          duration={25}
+          color="#BDE7FF"
+
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList
+          title="Recently completed session"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
