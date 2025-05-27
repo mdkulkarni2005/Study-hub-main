5201329e1fae957e580fbfde4b3f12f69fda52e0
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2025-05-27 07:33:04 +0530

    update getAllCompanions to handle subject and topic filters, and optimize imports in companion.actions.ts

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,18 +1,42 @@
-'use server';
+"use server";
 
-import {auth} from "@clerk/nextjs/server";
-import {createSupabaseClient} from "@/lib/supabase";
+import { auth } from "@clerk/nextjs/server";
+import { createSupabaseClient } from "@/lib/supabase";
 
 export const createCompanion = async (formData: CreateCompanion) => {
-    const { userId: author } = await auth();
+  const { userId: author } = await auth();
+  const supabase = createSupabaseClient();
+
+  const { data, error } = await supabase
+    .from("companions")
+    .insert({ ...formData, author })
+    .select();
+
+  if (error || !data)
+    throw new Error(error?.message || "Failed to create a companion");
+
+  return data[0];
+};
+
+export const getAllCompanions = async ({ limit = 10, page = 1, subject, topic }: GetAllCompanions) => {
     const supabase = createSupabaseClient();
 
-    const { data, error } = await supabase
-        .from('companions')
-        .insert({...formData, author })
-        .select();
+    let query = supabase.from('companions').select();
 
-    if(error || !data) throw new Error(error?.message || 'Failed to create a companion');
+    if(subject && topic) {
+        query = query.ilike('subject', `%${subject}%`)
+            .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    } else if(subject) {
+        query = query.ilike('subject', `%${subject}%`)
+    } else if(topic) {
+        query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`)
+    }
 
-    return data[0];
-}
+    query = query.range((page - 1) * limit, page * limit - 1);
+
+    const { data: companions, error } = await query;
+
+    if(error) throw new Error(error.message);
+
+    return companions;
+};
